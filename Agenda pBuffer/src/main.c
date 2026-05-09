4e2fc128f3379f122d576aff2ceb5fd62bad8b26
GustavoSBergmann <gustavos.bergmann@gmail.com>
2026-05-09 01:59:40 -0300

    Salvando por segurança

--- Agenda pBuffer/src/main.c
+++ Agenda pBuffer/src/main.c
@@ -2,47 +2,51 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MENU (sizeof(int))
-#define QTD_PESSOAS (MENU + sizeof(int))
-#define CONTADOR (QTD_PESSOAS + sizeof(int))
-#define DESLOCAMENTO (CONTADOR + sizeof(int))
-#define INICIO_AGENDA (DESLOCAMENTO + sizeof(char))
-#define TAM_NOME 100
-#define TAM_EMAIL 100
-#define TAM_PESSOA ((TAM_NOME * sizeof(char)) + sizeof(int) + (TAM_EMAIL * sizeof(char)))
+// Tamanhos para cada informação
+#define TAM_NOME 50
+#define TAM_EMAIL 50
+#define TAM_IDADE sizeof(int)
 
-void add(void **pBuffer, void **pAux);
+// Espaços para as variáveis inteiras de controle
+#define MENU(pBuffer) ((int *)pBuffer)
+#define QTD_PESSOAS(pBuffer) ((int *)pBuffer + 1)
+#define CONTADOR(pBuffer) ((int *)pBuffer + 2)
+#define DADOS_USADOS(pBuffer) ((int *)pBuffer + 3)
+#define TAM_PESSOA(pBuffer) ((int *)pBuffer + 4)
+
+// Tamanho do espaço para as variáveis de controle
+#define TAM_CONTROLE 5
+#define AREA_CONTROLE (sizeof(int) * TAM_CONTROLE)
+
+// Espaços para os buffers temporários (Nome, Email e Idade)
+#define TEMP_INICIO(pBuffer) ((void *)((int *)pBuffer + TAM_CONTROLE))
+#define TEMP_NOME(pBuffer) ((char *)TEMP_INICIO(pBuffer))
+#define TEMP_EMAIL(pBuffer) (TEMP_NOME(pBuffer) + TAM_NOME)
+#define TEMP_IDADE(pBuffer) ((int *)(TEMP_EMAIL(pBuffer) + TAM_EMAIL))
+
+// Tamanho do espaço para as variáveis temporárias
+#define TAM_AREA_TEMP (TAM_NOME + TAM_EMAIL + TAM_IDADE)
+
+// Começo da área de dados
+#define INICIO_AREA_DADOS (AREA_CONTROLE + TAM_AREA_TEMP)
+
+void iniciaBuffer(void **pBufferParametro);
+void menu(void *pBuffer);
+void add(void **pBufferParametro);
 
 int main()
 {
-    void *pBuffer = malloc(2 * sizeof(int));
-    void *pAux = NULL;
-    *(int *)(pBuffer + QTD_PESSOAS) = 0;
-    *(int *)(pBuffer + DESLOCAMENTO) = 0;
+    void *pBuffer;
+    iniciaBuffer(&pBuffer);
+    menu(pBuffer);
 
-    do
+    while (1)
     {
-        printf("*********** Selecione uma opcao do menu: ***********\n");
-        printf("1 - Adicionar Pessoa (Nome, Idade, Email)\n");
-        printf("2 - Remover Pessoa\n");
-        printf("3 - Buscar pessoa\n");
-        printf("4 - Listar todos\n");
-        printf("5 - Sair\n");
-        printf("Insira uma opcao do menu: ");
-        scanf(" %d", (int *)(pBuffer + MENU));
-
-        switch (*(int *)(pBuffer + MENU))
+        switch (*MENU(pBuffer))
         {
         case 1:
             printf("Adicionar pessoa (Nome, Idade, Email)\n");
-            add(&pBuffer, &pAux);
-            /*
-            for (int i = 0; i < *(int *)(pBuffer + QTD_PESSOAS); i++)
-            {
-                printf("Nome: %s\n", );
-            }
-            */
-            printf("Nome: %s\n", (char *)(pBuffer + INICIO_AGENDA));
+            add(&pBuffer);
             break;
 
         case 2:
@@ -59,85 +63,84 @@ int main()
 
         case 5:
             printf("Sair\n");
-            exit(0);
+            free(pBuffer);
+            return 0;
             break;
 
         default:
             break;
         }
-    } while (*(int *)(pBuffer + CONTADOR) != 5);
-
-    free(pBuffer);
-    free(pAux);
-    return 0;
+    }
 }
 
-void add(void **pBuffer, void **pAux)
+void iniciaBuffer(void **pBufferParametro)
 {
-    void *p = NULL;
+    // Inicia a área de variáveis de controle e o buffer temporário com 0
+    *pBufferParametro = calloc(1, INICIO_AREA_DADOS);
 
-    p = (char *)malloc(TAM_NOME * sizeof(char));
-    printf("Insira o nome da pessoa: ");
-    scanf(" %99s", (char *)p);
-    *pAux = realloc(*pBuffer, sizeof(*pBuffer) + (sizeof(char) * strlen((char *)p)));
-
-    // Verifica se a memória foi alocada
-    if (*pAux == NULL)
+    // Verifica se o ponteiro foi alocado
+    if (*pBufferParametro == NULL)
     {
         printf("Erro ao alocar!!!\n");
         exit(1);
     }
-    else
-    {
-        *pBuffer = *pAux;
-        *(int *)(*pBuffer + DESLOCAMENTO) += strlen((char *)p);
-        memcpy((char *)(*pBuffer + INICIO_AGENDA + *(int *)(*pBuffer + DESLOCAMENTO)),
-               (char *)p,
-               (sizeof(char) * strlen((char *)p)) + 1);
-        free(p);
-    }
 
-    p = (int *)malloc(sizeof(int));
-    printf("Insira a idade da pessoa: ");
-    scanf(" %d", (int *)p);
-    *pAux = realloc(*pBuffer, sizeof(*pBuffer) + sizeof(int));
+    // Local onde começa a área de dados
+    *DADOS_USADOS(*pBufferParametro) = INICIO_AREA_DADOS;
+}
 
-    // Verifica se a memória foi alocada
-    if (*pAux == NULL)
+void menu(void *pBuffer)
+{
+    do
     {
-        printf("Erro ao alocar!!!\n");
+        printf("*********** Selecione uma opcao do menu: ***********\n");
+        printf("1 - Adicionar Pessoa (Nome, Idade, Email)\n");
+        printf("2 - Remover Pessoa\n");
+        printf("3 - Buscar pessoa\n");
+        printf("4 - Listar todos\n");
+        printf("5 - Sair\n");
+        printf("Insira uma opcao do menu: ");
+        scanf(" %d", MENU(pBuffer));
+    } while (*MENU(pBuffer) < 1 || *MENU(pBuffer) > 5);
+}
+
+void add(void **pBufferParametro)
+{
+    void *pBuffer = *pBufferParametro;
+    void *bufferAux;
+
+    printf("----------- Adicionar pessoa -----------\n");
+    printf("Insira o nome: ");
+    scanf(" %49[^\n]s", TEMP_NOME(pBuffer));
+    printf("Insira o email: ");
+    scanf(" %49[^\n]s", TEMP_EMAIL(pBuffer));
+    printf("Insira a idade: ");
+    scanf(" %d", TEMP_IDADE(pBuffer));
+
+    // Cálculo do tamanho necessário para alocar essa nova pessoa:
+    // Tamanho do nome + \0 + Tamanho do Email + \0 + Tamanho da Idade(int)
+    *TAM_PESSOA(pBuffer) = ((strlen(TEMP_NOME(pBuffer)) + 1) + (strlen(TEMP_EMAIL(pBuffer)) + 1) + TAM_IDADE);
+
+    bufferAux = realloc(pBuffer, (*DADOS_USADOS(pBuffer) + *TAM_PESSOA(pBuffer)));
+
+    if (bufferAux == NULL)
+    {
+        printf("Erro de realocação!!!\n");
         exit(1);
     }
     else
     {
-        *pBuffer = *pAux;
-        *(int *)(*pBuffer + DESLOCAMENTO) += sizeof(int);
-        memcpy((char *)(*pBuffer + INICIO_AGENDA + *(int *)(*pBuffer + DESLOCAMENTO)),
-               (int *)p,
-               sizeof(int));
-        free(p);
+        pBuffer = bufferAux;
     }
 
-    p = (char *)malloc(TAM_EMAIL * sizeof(char));
-    printf("Insira o email da pessoa: ");
-    scanf(" %99s", (char *)p);
-    *pAux = realloc(*pBuffer, sizeof(*pBuffer) + (sizeof(char) * strlen((char *)p)));
+    memcpy(((char *)pBuffer + *DADOS_USADOS(pBuffer)), TEMP_NOME(pBuffer), strlen(TEMP_NOME(pBuffer)) + 1);
+    *DADOS_USADOS(pBuffer) += strlen(TEMP_NOME(pBuffer)) + 1;
 
-    // Verifica se a memória foi alocada
-    if (*pAux == NULL)
-    {
-        printf("Erro ao alocar!!!\n");
-        exit(1);
-    }
-    else
-    {
-        *pBuffer = *pAux;
-        *(int *)(*pBuffer + DESLOCAMENTO) += strlen((char *)p);
-        memcpy((char *)(*pBuffer + INICIO_AGENDA + *(int *)(*pBuffer + DESLOCAMENTO)),
-               (char *)p,
-               (sizeof(char) * strlen((char *)p)) + 1);
-        free(p);
-    }
+    memcpy(((char *)pBuffer + *DADOS_USADOS(pBuffer)), TEMP_EMAIL(pBuffer), strlen(TEMP_EMAIL(pBuffer)) + 1);
+    *DADOS_USADOS(pBuffer) += strlen(TEMP_EMAIL(pBuffer)) + 1;
 
-    *(int *)(*pBuffer + QTD_PESSOAS) += 1;
+    memcpy(((char *)pBuffer + *DADOS_USADOS(pBuffer)), TEMP_IDADE(pBuffer), TAM_IDADE);
+    *DADOS_USADOS(pBuffer) += TAM_IDADE;
+
+    (*QTD_PESSOAS(pBuffer))++;
 }
